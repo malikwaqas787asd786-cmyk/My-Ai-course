4284228dc46707905c6649d35393b35857d00930
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-02 10:21:53 +0500

    program5.py

---
+++ 3rdprogram.py
@@ -0,0 +1,29 @@
+str1="This is apna."
+print(str1)
+str2="This is waqas.waqas is a man."
+print(str2)
+str3="My name is waqas.\n i am a muslim."
+print(str3)
+str3="My name is waqas.\t i am a muslim."
+print(str3)
+str4="waqas"
+str5="muslim"
+final_str = str4+str5
+print(final_str)
+str6="waqas"
+str7="muslim"
+print(len(str6))
+print(len(str7))
+len1=len(str6)
+len2=len(str7)
+final_str = str6+str7
+print(final_str)
+print(len(final_str))
+str="waqas"
+ch = str[2]
+print(ch)
+str1="waqas"
+print(str[1:5])
+print(str[:4]) 
+print(str[5:])
+ 
