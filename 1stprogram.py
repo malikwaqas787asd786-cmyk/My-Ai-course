ea5f69f9aa75a5b2e2181ecddfeb4b9715bc1930
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-02 10:19:26 +0500

    program6.py

---
+++ 1stprogram.py
@@ -0,0 +1,29 @@
+name="waqas"
+age=20
+price=25.5
+print(type(name))
+print(type(age))
+print(type(price))
+
+print(name)
+print(age)
+print(price)
+
+print("my name is:","waqas")
+print("my age is:",20)
+print("the price is:",25.5)
+name="waqas"
+age=20
+price=25.5
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+print(name)
+print(age)
+print(price)
+
+print("my name is:","waqas")
+print("my age is:",20)
+print("the price is:",25.5)
