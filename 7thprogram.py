91a0f315e5f143f8eaa0aee3fc29d12e511a9ad5
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-05 15:55:16 +0500

    7thprogram.py

---
+++ 7thprogram.py
@@ -0,0 +1,9 @@
+# wap to input 2 int numbers a and b.print true if 
+# a and b print true if a is greaten then or equal
+# to if not print false ?
+
+# a>=b
+a=int(input("enter first:"))
+b=int(input("enter second:"))
+
+print(a>=b)
