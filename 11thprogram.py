e3107dbb8ef8960fec5f8d694fc403da456ab70c
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-07 20:18:01 +0500

    11program.py

---
+++ 11thprogram.py
@@ -0,0 +1,13 @@
+# wap to ask the user to enter names of their 3 favorite
+# movies and store them in list?
+
+movies=[]
+movie1=input("enter fisrt movie:")
+movie2=input('enter second movie:')
+movie3=input("enter third movie:")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
