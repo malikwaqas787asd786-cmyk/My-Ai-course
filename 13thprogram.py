e26ea57f009d4b8ae67bf2dd6ead6fc71bffc610
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-07 20:23:54 +0500

    13thprogram.py

---
+++ 13thprogram.py
@@ -0,0 +1,8 @@
+# wap to count the numbers of the students with the "A" grade in the fllowing tuple?
+
+# the grade of the students:.
+                            #  ["a","a","b","c","d"]
+
+grade=("a","a","b","c","d")
+
+print(grade.count("a"))
