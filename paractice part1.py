925203f435b140d54f44ae69f0b08df816800bfa
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-02-22 11:01:50 +0500

    part1.py

---
+++ paractice part1.py
@@ -0,0 +1,41 @@
+# # Q1
+# print("hello world")
+# name="waqas"
+# print(name)
+
+# # Q2
+# color=input("enter your color:")
+# print(color)
+
+# Q3
+# print("ali","saif","waqas",sep="-")
+
+# Q3
+# age=int(input("enter your birth year:"))
+# print(2026-age)
+
+# Q5
+# sum=5+5
+# print(sum)
+
+# Q6
+# print("malik", end=" ")
+# print("waqas")
+
+# Q7
+# name=input("enter first name:")
+# name2=input("enter second name:")
+# print(name+"  "+name2)
+
+# Q8
+# name=input("enter your name:")
+# print(f"WELCOME,{name.upper()}!")
+
+# Q9
+# city=input("enter your city:")
+# country=input("enter your country:")
+# print(f"{city},{country}")
+
+# Q10
+# age=19
+# print("your age is:"  +str(age))
