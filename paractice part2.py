8c7b980be0726062e80c138793b5797ff63a5f5a
malikwaqas787asd786-cmyk <malikwaqas787asd786@gmail.com>
2026-03-07 10:37:34 +0500

    practice part2.py

---
+++ paractice part2.py
@@ -0,0 +1,30 @@
+# Q1
+# age=19
+# height=5.8
+# print(type(age))
+# print(type(height))
+
+# Q2
+# num=3+4j
+# print(num)
+# print(type(num))
+
+# Q3
+# is_python_fun=True
+# print(is_python_fun)
+# print(type(is_python_fun))
+
+# Q4
+# a=10
+# b=20
+# c=30
+# print(a,b,c)
+
+# Q5
+# x=100
+# y=100
+# z=100
+# print(x,y,z)
+
+# Q6
+# m
